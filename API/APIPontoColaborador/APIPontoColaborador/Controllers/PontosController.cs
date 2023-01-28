042fb997ffaba17ad95ef42c856a103caff81bd5
Hudson Retonde <hudsonsretonde@gmail.com>
2023-01-28 18:11:44 -0300

    Retornando todos os pontos da tabela

--- API/APIPontoColaborador/APIPontoColaborador/Controllers/PontosController.cs
+++ API/APIPontoColaborador/APIPontoColaborador/Controllers/PontosController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APIPontoColaborador.Context;
+using APIPontoColaborador.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIPontoColaborador.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PontosController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public PontosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Ponto>> Get() 
+        {
+            var pontos = _context.Pontos.ToList();
+            if (pontos is null)
+            {
+                return NotFound("Ponto não encontrado.");
+            }
+            return pontos;
+        }
     }
 }
