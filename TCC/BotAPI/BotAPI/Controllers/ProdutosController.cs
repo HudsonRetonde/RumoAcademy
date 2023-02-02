879232cf1da7f5f1146a96ab02cc66ff02e8a4ff
Hudson Retonde <hudsonsretonde@gmail.com>
2023-02-02 19:43:34 -0300

    Melhorando o tratamento de erros.

--- TCC/BotAPI/BotAPI/Controllers/ProdutosController.cs
+++ TCC/BotAPI/BotAPI/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using BotAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BotAPI.Controllers
 {
@@ -19,31 +20,75 @@ namespace BotAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
-            if (produtos is null)
-                return NotFound("Produtos não encontrados...");
-            return produtos;
+            try
+            {
+                var produtos = _context.Produtos.AsNoTracking().ToList();
+                if (produtos is null)
+                    return NotFound("Produtos não encontrados...");
+                return produtos;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solicitação.");
+            }
         }
 
         [HttpGet("{id:int}", Name= "ObterProduto")]
         public ActionResult<Produto> Get(int id) 
-        { 
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId== id);
-            if (produto is null)
-                return NotFound("Id não encontrado, por gentileza, verifique se o número está correto...");
-            return produto;
+        {
+            try
+            {
+                var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+                if (produto is null)
+                    return NotFound("Id não encontrado, por gentileza, verifique se o número está correto...");
+                return produto;
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solicitação.");
+            }
         }
 
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
             if (produto is null)
-                return BadRequest("Produto não encontrado..."); 
+                return BadRequest("Houve algum erro, por favor verifique se todos os itens foram preenchidos da forma correta..."); 
             _context.Produtos.Add(produto);
             _context.SaveChanges();
 
             return new CreatedAtRouteResult("ObterProduto",
                 new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto) 
+        {
+            if (id != produto.ProdutoId)
+                return BadRequest($"Produto com o id {id} não encontrado. Verifique se está correto...");
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id) 
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+                return NotFound($"Produto com o id {id} não encontrado. Verifique se está correto...");
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
 
     }
